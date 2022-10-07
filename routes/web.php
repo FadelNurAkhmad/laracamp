16e1e64d4ba64e5a2c000a5a61b3fa7f0dbb5d03
FadelNurAkhmad <fadelnurakhmad1@gmail.com>
2022-10-07 22:14:17 +0700

    slicing landing page, login checkout, success

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () { // nama di tag url
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('login', function () {
+    return view('login'); // kembali ke 'login.blade.php'
+})->name('login'); // name buat 'route' di blade
+
+Route::get('checkout', function () {
+    return view('checkout'); // kembali ke 'checkout.blade.php'
+})->name('checkout'); // name buat 'route' di blade
+
+Route::get('success-checkout', function () {
+    return view('success_checkout'); // kembali ke 'success_checkout.blade.php'
+})->name('success-checkout'); // name buat 'route' di blade
+
+Route::get('dashboard', function () {
+    return view('dashboard'); // kembali ke 'dashboard.blade.php'
+})->name('dashboard'); // name buat 'route' di blade
